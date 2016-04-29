34abb29b81ebb909ce621ca981c5f8e57611233a
Tyson Bailey <onaclov2000+github@gmail.com>
2016-04-28 23:13:18 -0600

    Update subscribe.py

--- hangupsbot/plugins/subscribe.py
+++ hangupsbot/plugins/subscribe.py
@@ -42,6 +42,11 @@ def _handle_keyword(bot, event, command):
                     regexphrase = "\\b" + phrase + "\\b"
                     if re.search(regexphrase, event.text, re.IGNORECASE):
                         yield from _send_notification(bot, event, phrase, user)
+                        break # Stop looking for keywords we're already sending this message to the user.
+                              # The only meh part is that we can't combine all the phrases we saw in a single line
+                              # so really whichever is the first it see's it's going to send that as the phrase it was
+                              # looking for. I'm open to suggestions, however I assume we need to look at event.text 
+                              # and see if we could perhaps find all the phrases and comma separate list them... but why?
         except KeyError:
             # User probably hasn't subscribed to anything
             continue
